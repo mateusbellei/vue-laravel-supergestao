93cfbbcfd149e0bc6b7c2203844bed53dfdd8563
teuzovisk <germano_sky@hotmail.com>
2021-12-13 00:11:56 -0300

    first commit

---
+++ app/Http/Controllers/ContatoController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\SiteContato;
+use App\MotivoContato;
+
+class ContatoController extends Controller
+{
+    public function contato(Request $request) {
+
+        $motivo_contatos = MotivoContato::all();
+
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
+    }
+
+    public function salvar(Request $request) {
+
+        //realizar a validação dos dados do formulário recebidos no request
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'email',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
+        // SiteContato::create($request->all());
+    }
+}
---
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -0,0 +1,28 @@
+{{ $slot }}
+<form action={{ route('site.contato') }} method="post">
+    @csrf
+    <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    <br>
+    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    <br>
+    <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    <br>
+
+    <select name="motivo_contato" class="{{ $classe }}">
+        <option value="">Qual o motivo do contato?</option>
+
+        @foreach($motivo_contatos as $key => $motivo_contato)
+            <option value="{{$motivo_contato->id}}" {{ old('motivo_contato') == $motivo_contato->id ? 'selected' : '' }}>{{$motivo_contato->motivo_contato}}</option>
+        @endforeach
+    </select>
+    <br>
+    <textarea name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem' }}</textarea>
+    <br>
+    <button type="submit" class="{{ $classe }}">ENVIAR</button>
+</form>
+
+<div style="position:absolute; top:0px; width:100%; background:red">
+    <pre>
+    {{ print_r($errors) }}
+    </pre>
+</div>
